240f467cd26d351bd50a86c5b4229fae50aa8891
Simon Martin <smartin@scottlogic.com>
2023-03-24 13:46:35 +0000

    Lift setup from fuse_mt example

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use fuse_mt::spawn_mount;
 fn main() {
     println!("Hello, world!");
 
+    let fuse_args = [OsStr::new("-o"), OsStr::new("fsname=organizefs")];
+
     let fs = organizefs::OrganizeFS::new();
-    spawn_mount(fs, "", &[]);
+    spawn_mount(fuse_mt::FuseMT::new(fs, 1), "", &fuse_args[..]);
 }
