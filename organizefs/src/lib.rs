63f0235e8ccd9ff592bad050f8ec90a71df511c5
Simon Martin <smartin@scottlogic.com>
2023-03-29 07:22:39 +0100

    Start of componentization

--- organizefs/src/lib.rs
+++ organizefs/src/lib.rs
@@ -14,6 +14,8 @@ use humansize::FormatSize;
 use itertools::Itertools;
 use tracing::{debug, error, info};
 use walkdir::WalkDir;
+use common::Normalize;
+
 mod libc_wrapper;
 
 #[derive(Debug)]
@@ -87,50 +89,6 @@ impl<'a> From<std::path::Component<'a>> for Component {
             std::path::Component::ParentDir => todo!(),
             std::path::Component::Normal(p) => Component::Normal(p.to_os_string()),
         }
-    }
-}
-
-trait Normalize {
-    fn normalize(&self) -> Self;
-}
-
-impl Normalize for PathBuf {
-    fn normalize(&self) -> Self {
-        let mut comps = Vec::new();
-
-        for c in self.components() {
-            match c {
-                std::path::Component::Prefix(_) => todo!(),
-                std::path::Component::RootDir => {
-                    comps.clear();
-                    comps.push(c);
-                }
-                std::path::Component::CurDir => {}
-                std::path::Component::ParentDir => {
-                    if let Some(std::path::Component::Normal(_)) = comps.last() {
-                        comps.pop();
-                    }
-                }
-                std::path::Component::Normal(p) => comps.push(c),
-            }
-        }
-        let mut res = OsString::new();
-        let mut need_sep = false;
-
-        for c in comps {
-            if need_sep && c != std::path::Component::RootDir {
-                res.push(std::path::MAIN_SEPARATOR_STR);
-            }
-            res.push(c.as_os_str());
-
-            need_sep = match c {
-                std::path::Component::RootDir => false,
-                std::path::Component::Prefix(prefix) => todo!(),
-                _ => true,
-            }
-        }
-        debug!(source = debug(self), target = debug(&res), "normalize");
-        PathBuf::from(&res)
     }
 }
 
