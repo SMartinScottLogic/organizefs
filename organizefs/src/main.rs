63862b26b8621c94692bc338d755d6ee122bb032
Simon Martin <havvoric@gmail.com>
2023-04-04 03:38:08 +0100

    Configure log level from RUST_LOG env variable.

--- organizefs/src/main.rs
+++ organizefs/src/main.rs
@@ -1,18 +1,27 @@
 use axum::{routing::get, Router};
 use fuse_mt::{spawn_mount, FuseMT};
 use organizefs::OrganizeFS;
-use std::{env, ffi::OsStr};
+use std::{env, ffi::OsStr, str::FromStr};
+use tracing::Level;
 use tracing_subscriber::fmt::format::FmtSpan;
 
 #[tokio::main]
 async fn main() {
     // install global collector configured based on RUST_LOG env var.
+    let level = match env::var("RUST_LOG") {
+        Ok(v) => match Level::from_str(&v) {
+            Ok(l) => l,
+            Err(_) => Level::INFO,
+        },
+        Err(_) => Level::INFO,
+    };
     tracing_subscriber::fmt()
         .with_span_events(FmtSpan::NONE)
         .with_thread_ids(true)
         .with_thread_names(true)
         .with_file(true)
         .with_line_number(true)
+        .with_max_level(level)
         .init();
 
     let args: Vec<String> = env::args().collect();
