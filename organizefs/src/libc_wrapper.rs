7031ee3285fca34d55d3b36cfcd71f35a8540a70
Simon Martin <havvoric@gmail.com>
2023-04-05 07:16:48 +0100

    Add file reading

--- organizefs/src/libc_wrapper.rs
+++ organizefs/src/libc_wrapper.rs
@@ -6,6 +6,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
+use libc::c_void;
 use tracing::error;
 
 pub fn statfs(path: &PathBuf) -> io::Result<libc::statfs> {
@@ -51,4 +52,52 @@ pub fn lstat(path: &PathBuf) -> io::Result<libc::stat> {
         let stat = unsafe { stat.assume_init() };
         Ok(stat)
     }
+}
+
+pub fn open(path: &PathBuf, flags: i32) -> io::Result<i32> {
+    let cstr = CString::new(path.clone().into_os_string().as_bytes())?;
+    let result = unsafe { libc::open(cstr.as_ptr(), flags) };
+    if -1 == result {
+        let e = io::Error::last_os_error();
+        error!("open({:?}): {}", path, e);
+        Err(e)
+    } else {
+        Ok(result)
+    }
+}
+
+pub fn close(fd: i32) -> io::Result<()> {
+    let result = unsafe { libc::close(fd) };
+    if -1 == result {
+        let e = io::Error::last_os_error();
+        error!("close({:?}): {}", fd, e);
+        Err(e)
+    } else {
+        Ok(())
+    }
+}
+
+pub fn read(fd: i32, offset: i64, count: u32) -> io::Result<Vec<u8>> {
+    let result = unsafe { libc::lseek64(fd, offset, libc::SEEK_SET) };
+    if -1 == result {
+        let e = io::Error::last_os_error();
+        error!("read({:?}): {}", fd, e);
+        return Err(e);
+    }
+    let mut buf = Vec::new();
+    buf.resize(count.try_into().unwrap(), 0);
+
+    let result = unsafe {
+        libc::read(
+            fd,
+            buf.as_mut_ptr() as *mut c_void,
+            count.try_into().unwrap(),
+        )
+    };
+    if -1 == result {
+        let e = io::Error::last_os_error();
+        error!("read({:?}): {}", fd, e);
+        return Err(e);
+    }
+    Ok(buf)
 }
